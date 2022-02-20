e94927e5d7e174c1b8433ad7d882449c1d4b8a80
Sean Sundberg <seansund@us.ibm.com>
2022-02-20 16:13:48 -0600

    Updates handling of external data source inputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/get-cluster.sh
+++ scripts/get-cluster.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-env
+INPUT=$(tee)
 
-eval "$(${bin_dir}/jq -r '@sh "CLUSTER_NAME=\(.cluster_name) RESOURCE_GROUP_NAME=\(.resource_group_name) SUBSCRIPTION_ID=\(.subscription_id) TENANT_ID=\(.tenant_id) CLIENT_ID=\(.client_id)" CLIENT_SECRET=\(.client_secret)')"
+BIN_DIR=$(echo "${INPUT}" | sed -E 's/"bin_dir": ?"([^"]+)"/\1/g')
+
+eval "$(echo "$INPUT" | ${BIN_DIR}/jq -r '@sh "CLUSTER_NAME=\(.cluster_name) RESOURCE_GROUP_NAME=\(.resource_group_name) SUBSCRIPTION_ID=\(.subscription_id) TENANT_ID=\(.tenant_id) CLIENT_ID=\(.client_id)" CLIENT_SECRET=\(.client_secret)')"
 
 API_VERSION="2019-04-30"
 
@@ -13,14 +15,14 @@ API_VERSION="2019-04-30"
 #TENANT_ID=""
 
 echo "Getting token"
-TOKEN=$(curl -sX POST -d "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&resource=https%3A%2F%2Fmanagement.azure.com%2F" "https://login.microsoftonline.com/${TENANT_ID}/oauth2/token")
+TOKEN=$(curl -s -X POST -d "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&resource=https%3A%2F%2Fmanagement.azure.com%2F" "https://login.microsoftonline.com/${TENANT_ID}/oauth2/token")
 
 echo "Got token: ${TOKEN}"
 
 URL="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.ContainerService/openShiftManagedClusters/${CLUSTER_NAME}?api-version=${API_VERSION}"
 
-curl -sX GET \
+curl -s -X GET \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json" \
   "${URL}" | \
-  ${bin_dir}/jq '{id: .id, location: .location, fqdn: .properties.fqdn, publicHostname: .properties.publicHostname, router_fqdn: .properties.routerProfiles[0].fqdn, publicSubdomain: .properties.routerProfiles[0].publicSubdomain}'
+  ${BIN_DIR}/jq '{id: .id, location: .location, fqdn: .properties.fqdn, publicHostname: .properties.publicHostname, router_fqdn: .properties.routerProfiles[0].fqdn, publicSubdomain: .properties.routerProfiles[0].publicSubdomain}'
