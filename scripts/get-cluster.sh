f305070089682b2df7be530eb6ee8554178e0cc4
Sean Sundberg <seansund@us.ibm.com>
2022-02-21 13:15:07 -0600

    Fixes get-cluster logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/get-cluster.sh
+++ scripts/get-cluster.sh
@@ -4,10 +4,9 @@ set -e
 
 INPUT=$(tee)
 
-BIN_DIR=$(echo "${INPUT}" | sed -E 's/.*"bin_dir": ?"([^"]+)".*/\1/g')
-TMP_DIR=$(echo "${INPUT}" | sed -E 's/.*"tmp_dir": ?"([^"]+)".*/\1/g')
+BIN_DIR=$(echo "${INPUT}" | grep "bin_dir" | sed -E 's/.*"bin_dir": ?"([^"]+)".*/\1/g')
 
-eval "$(echo "$INPUT" | ${BIN_DIR}/jq -r '@sh "CLUSTER_NAME=\(.cluster_name) RESOURCE_GROUP_NAME=\(.resource_group_name) SUBSCRIPTION_ID=\(.subscription_id) TENANT_ID=\(.tenant_id) CLIENT_ID=\(.client_id)" CLIENT_SECRET=\(.client_secret)')"
+eval "$(echo "${INPUT}" | ${BIN_DIR}/jq -r '@sh "CLUSTER_NAME=\(.cluster_name) RESOURCE_GROUP_NAME=\(.resource_group_name) SUBSCRIPTION_ID=\(.subscription_id) TENANT_ID=\(.tenant_id) CLIENT_ID=\(.client_id) CLIENT_SECRET=\(.client_secret) TOKEN=\(.access_token) TMP_DIR=\(.tmp_dir)"')"
 
 API_VERSION="2020-04-30"
 
@@ -20,7 +19,9 @@ mkdir -p "${TMP_DIR}"
 #CLIENT_SECRET=""
 #TENANT_ID=""
 
-TOKEN=$(curl -s -X POST -d "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&resource=https%3A%2F%2Fmanagement.azure.com%2F" "https://login.microsoftonline.com/${TENANT_ID}/oauth2/token" | ${BIN_DIR}/jq -r '.access_token')
+if [[ -z "${TOKEN}" ]]; then
+  TOKEN=$(curl -s -X POST -d "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&resource=https%3A%2F%2Fmanagement.azure.com%2F" "https://login.microsoftonline.com/${TENANT_ID}/oauth2/token" | ${BIN_DIR}/jq -r '.access_token')
+fi
 
 URL="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.RedHatOpenshift/openShiftClusters/${CLUSTER_NAME}?api-version=${API_VERSION}"
 
@@ -32,9 +33,10 @@ curl -s -X GET \
 
 AUTH_URL="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.RedHatOpenShift/openShiftClusters/${CLUSTER_NAME}/listCredentials?api-version=${API_VERSION}"
 
-curl -s -X GET \
+curl -s -X POST \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json" \
+  -d "" \
   "${AUTH_URL}" > "${TMP_DIR}/credentials.json"
 
 jq -s '.[0] * .[1]' "${TMP_DIR}/output.json" "${TMP_DIR}/credentials.json"
