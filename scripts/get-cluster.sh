eb67b670ab273a314a362607f851a20fdead8e0a
Sean Sundberg <seansund@us.ibm.com>
2022-02-21 14:00:46 -0600

    Outputs bin dir
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/get-cluster.sh
+++ scripts/get-cluster.sh
@@ -6,6 +6,7 @@ INPUT=$(tee)
 
 BIN_DIR=$(echo "${INPUT}" | grep "bin_dir" | sed -E 's/.*"bin_dir": ?"([^"]+)".*/\1/g')
 
+echo "Bin dir: ${BIN_DIR}"
 echo "Input: ${INPUT}"
 
 eval "$(echo "${INPUT}" | ${BIN_DIR}/jq -r '@sh "CLUSTER_NAME=\(.cluster_name) RESOURCE_GROUP_NAME=\(.resource_group_name) SUBSCRIPTION_ID=\(.subscription_id) TENANT_ID=\(.tenant_id) CLIENT_ID=\(.client_id) CLIENT_SECRET=\(.client_secret) TOKEN=\(.access_token // empty) TMP_DIR=\(.tmp_dir)"')"
