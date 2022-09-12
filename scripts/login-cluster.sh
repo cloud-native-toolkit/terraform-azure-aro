b03c4ac5f63868663915174fe947bfb0af2dbe0c
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-09-13 09:42:53 +1000

    Adds insecure login to avoid wait for certificate
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- scripts/login-cluster.sh
+++ scripts/login-cluster.sh
@@ -8,7 +8,7 @@ BIN_DIR=$(echo "${INPUT}" | grep "bin_dir" | sed -E 's/.*"bin_dir": ?"([^"]+)".*
 
 eval "$(echo "${INPUT}" | ${BIN_DIR}/jq -r '@sh "SERVER_URL=\(.server_url) USERNAME=\(.username) PASSWORD=\(.password) KUBECONFIG=\(.kubeconfig)"')"
 
-oc login ${SERVER_URL} --username=${USERNAME} --password=${PASSWORD} --kubeconfig=${KUBECONFIG} 1> /dev/null 2> /dev/null
+oc login ${SERVER_URL} --username=${USERNAME} --password=${PASSWORD} --kubeconfig=${KUBECONFIG} --insecure-skip-tls-verify 1> /dev/null 2> /dev/null
 
 SERVER=$(echo ${SERVER_URL} | sed 's/https\:\/\///g' | sed 's/\./-/g' | sed 's/\///g')
 
