ad2967b3accc0c7f3dbdffa17437a1435abdd303
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-09-12 11:11:26 +1000

    Fixes server name calculation for token regex
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- scripts/login-cluster.sh
+++ scripts/login-cluster.sh
@@ -8,10 +8,9 @@ BIN_DIR=$(echo "${INPUT}" | grep "bin_dir" | sed -E 's/.*"bin_dir": ?"([^"]+)".*
 
 eval "$(echo "${INPUT}" | ${BIN_DIR}/jq -r '@sh "SERVER_URL=\(.server_url) USERNAME=\(.username) PASSWORD=\(.password) KUBECONFIG=\(.kubeconfig)"')"
 
-
 oc login ${SERVER_URL} --username=${USERNAME} --password=${PASSWORD} --kubeconfig=${KUBECONFIG} 1> /dev/null 2> /dev/null
 
-SERVER=$(echo ${SERVER_URL} | sed 's/https\:\/\///g' | sed 's/\./-/g')
+SERVER=$(echo ${SERVER_URL} | sed 's/https\:\/\///g' | sed 's/\./-/g' | sed 's/\///g')
 
 TOKEN=$(cat "${KUBECONFIG}" | yq eval ".users[] | select(.name == \"*kube:admin/${SERVER}\") | .user.token")
 
