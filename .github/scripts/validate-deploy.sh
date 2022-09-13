12da7c6cff9fe955190ed55cc4ece801e1859cd1
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-09-14 08:42:43 +1000

    Adds output of kubeconfig to temp file
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-export KUBECONFIG=$(terraform output -raw config_file_path)
+# Below is due to file length error with oc when running on github runner
+cat $(terraform output -raw config_file_path) > /tmp/kubeconfig
+
+export KUBECONFIG="/tmp/kubeconfig"
 
 oc get nodes
+
+rm /tmp/kubeconfig
